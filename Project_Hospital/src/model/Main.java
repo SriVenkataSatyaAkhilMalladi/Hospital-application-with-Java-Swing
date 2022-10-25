2b32b02c8213ab91ee634833ecbde7672ef08b9f
Akhil Malladi <114646880+SriVenkataSatyaAkhilMalladi@users.noreply.github.com>
2022-10-24 21:22:17 -0400

    Add files via upload

---
+++ Project_Hospital/src/model/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author srivenkatasatyaakhilmalladi
+ */
+public class Main {
+    private String user_id;
+    private String password;
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public String getPassword() {
+        return password;
+    } 
+    
+}
