9f7f264a1b16c8d5d4443ad59b8d9adb7bd83a6f
Akhil Malladi <114646880+SriVenkataSatyaAkhilMalladi@users.noreply.github.com>
2022-10-26 04:11:18 -0400

    Add files via upload

---
+++ Project_Hospital3.0/src/model/Person.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author srivenkatasatyaakhilmalladi
+ */
+public class Person {
+    private int age;
+    private String name;
+    private String gender;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    
+    
+}
